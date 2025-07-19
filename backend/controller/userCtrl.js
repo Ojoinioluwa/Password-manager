bf721fe746073b63686a2e3b1931baac24e2db25
Ojay <ojoinioluwa@gmail.com>
2025-07-19 07:17:39 +0100

    Fixed critical bug in the verifyUser handler

--- backend/controller/userCtrl.js
+++ backend/controller/userCtrl.js
@@ -84,7 +84,7 @@ const userController = {
         if (user.expiresAt < new Date()) {
             await UserVerification.findByIdAndDelete(user._id)
             sendMail({
-                _id: req.user.id,
+                _id: userInfo._id,
                 email: userInfo.email,
                 firstName: userInfo.firstName
             })
